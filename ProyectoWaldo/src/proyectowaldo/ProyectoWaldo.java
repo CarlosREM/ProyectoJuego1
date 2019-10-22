6b7ba32adbf6a61427f5465ed234248c83b34551
Fabricio Ceciliano <fabricio.ceciliano.n@gmail.com>
2019-10-22 15:43:16 -0600

    Initial commit. Created Netbeans project

---
+++ ProyectoWaldo/src/proyectowaldo/ProyectoWaldo.java
@@ -0,0 +1,16 @@
+package proyectowaldo;
+
+/**
+ *
+ * @author Fabricio Ceciliano
+ */
+public class ProyectoWaldo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
